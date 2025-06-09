356e6b2b92e3c305e5647a13ff53f5a318a76e44
PiyushP <piyushp@Piyushs-MacBook-Air.local>
2025-06-09 16:05:37 -0400

    Better Rest Project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by PiyushP on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRestTests/BetterRestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestTests.swift
+//  BetterRestTests
+//
+//  Created by PiyushP on 6/8/25.
+//
+
+import Testing
+@testable import BetterRest
+
+struct BetterRestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
